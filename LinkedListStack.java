812a6260efcf4cba64d84bb5b82482e9c97203d9
Subodha Sathyangi <subosathya2003@gmail.com>
2026-01-05 20:19:56 +0530

    Add LinkedListStack implementation

---
+++ LinkedListStack.java
@@ -0,0 +1,59 @@
+class LinkedListStack {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node top = null;
+
+    // Push operation
+    void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        System.out.println("Pushed: " + data);
+    }
+
+    // Pop operation
+    void pop() {
+        if (top == null) {
+            System.out.println("Stack Underflow");
+            return;
+        }
+        System.out.println("Popped: " + top.data);
+        top = top.next;
+    }
+
+    // Display stack
+    void display() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return;
+        }
+
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack elements:");
+        stack.display();
+
+        stack.pop();
+        stack.display();
+    }
+}
