6e35e18e9796c1dbc989f164b66954769ed22b06
Subodha Sathyangi <subosathya2003@gmail.com>
2026-01-13 19:07:43 +0530

    Implement Insertion Sort algorithm in Java

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+
+    // Function to perform Insertion Sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    // Function to print array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+
+        insertionSort(arr);
+        printArray(arr);
+    }
+}
